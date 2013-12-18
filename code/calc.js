81dbbb341d07a1bc65e2784c42228a5dd2ccb8f4
Sy Le <synle@synle.com>
2013-12-18 11:25:17 -0800

    code coverage demo and calc change
    added good sample code to demonstrate code coverage and change calc
    class to fail when number is greater than 1000

--- code/calc.js
+++ code/calc.js
@@ -1,8 +1,23 @@
+/*
+	sample calculate util used for testing,
+	
+	this calculate will failed when either number is greater than 1000
+*/
 var Calc = {
 	plus : function (a, b) {
+		if (a > 1000 || b > 1000)
+			throw 'Calc.plus does not supported number greater than 1000';
+			
+		//sample code block that will never be covered
+		if (false == true){
+			var res = a + b + b + a;
+			return a + b + b + a;
+		}
 		return a + b;
 	},
 	minus : function (a, b) {
+		if (a > 1000 || b > 1000)
+			throw 'Calc.minus does not supported number greater than 1000';
 		return a - b;
 	}
 }
--- test/failed-test.js
+++ test/failed-test.js
@@ -1,8 +1,10 @@
 module("failed-test.js");
 test("Calc plus failed test", function () {
-	equal(1, 0);
+	equal(Calc.plus(1000, 2), 3);
+	equal(Calc.plus(2, 23423423), 3);
 });
 
 test("Calc minus failed test", function () {
-	equal(1, 2);
+	equal(Calc.minus(1000, 2), 3);
+	equal(Calc.minus(2, 23423423), 3);
 });
