581eb0c31cf333560a4a13258429792fe1abe901
Sy Le <synle@synle.com>
2013-12-20 13:47:12 -0800

    calc and random

--- code/calc.js
+++ code/calc.js
@@ -6,7 +6,7 @@
 var Calc = {
 	plus : function (a, b) {
 		if (a > 1000 || b > 1000)
-			throw 'Calc.plus does not supported number greater than 1000';
+			return 'Calc.plus does not support operand > 1000, a = ' + a + ', b = ' + b;
 			
 		//sample code block that will never be covered
 		if (false == true){
@@ -17,7 +17,7 @@ var Calc = {
 	},
 	minus : function (a, b) {
 		if (a > 1000 || b > 1000)
-			throw 'Calc.minus does not supported number greater than 1000';
+			return  'Calc.minus does not support operand > 1000, a = ' + a + ', b = ' + b;
 		return a - b;
 	}
 }
--- test/calc-random-test.js
+++ test/calc-random-test.js
@@ -1,16 +1,21 @@
 module("test.js");
+var max = 1050, tries = 5;
 test("Calc.plus", function () {
-	for (var i = 0; i < 50; i++){
-		var a = parseInt(Math.random() * 10000);
-		var b = parseInt(Math.random() * 10000);
+	for (var i = 0; i < tries; i++){
+		var a = parseInt(Math.random() * max);
+		var b = parseInt(Math.random() * max);
+
+		console.log('add', a, b);
 		equal(Calc.plus(a, b), a + b);
 	}	
 });
 
 test("Calc minus", function () {
-	for (var i = 0; i < 50; i++){
-		var a = parseInt(Math.random() * 10000);
-		var b = parseInt(Math.random() * 10000);
+	for (var i = 0; i < tries; i++){
+		var a = parseInt(Math.random() * max);
+		var b = parseInt(Math.random() * max);
+
+		console.log('minus', a, b);
 		equal(Calc.minus(a, b), a - b);
 	}	
 });
